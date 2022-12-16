e51caf49127d51126226c96c80b1d8c4c04740af
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2022-12-16 18:23:39 +0100

    Evaluación (II)
    Añade funcionalidad 'Eliminar' gasto
    Limpiar inputs 'Nombre' y 'Cantidad' al añadir/eliminar gasto
    Añade 'README.md'

--- modulo-1/evaluacion/index.js
+++ modulo-1/evaluacion/index.js
@@ -2,35 +2,38 @@
 const list = JSON.parse(localStorage.getItem('List Expenses')) || [];
 
 const addExpense = () => {
-    const name = document.querySelector('#name');
-    const quantity = document.querySelector('#quantity');
+    let name = document.querySelector('#name');
+    let quantity = document.querySelector('#quantity');
 
     if (name.value !== '' && quantity.value !== '') {
-        list.push({ name: name.value, quantity: quantity.value });
+        list.push({ name: name.value, quantity: Number(quantity.value) });
         localStorage.setItem('List Expenses', JSON.stringify(list));
+        name.value = '';
+        quantity.value = '';
     } else {
         alert('Hay algún campo vacío!');
     }
-
     console.log(list);
+    showCompleteList();
 };
 
 const removeExpense = () => {
     const name = document.querySelector('#name');
+    let product = list.find((item) => item.name.toLowerCase() === name.value.toLowerCase());
 
-    const newList = list.filter((item, index) => {
-        if (item.name.toLowerCase() === name.value.toLowerCase()) {
-            return list.slice(index, 1);
-        } else {
-            console.log('NO hay coincidencia');
-        }
-    });
-    console.log(newList);
-    return newList;
+    if (product) {
+        let position = list.indexOf(product);
+        list.splice(position, 1);
+        localStorage.setItem('List Expenses', JSON.stringify(list));
+        name.value = '';
+        quantity.value = '';
+    } else {
+        alert('No existe este ticket en tu listado.');
+    }
+    showCompleteList();
 };
 
 const showCompleteList = () => {
-    console.log(list.map((item) => item.name));
     const table = document.querySelector('#table');
     table.innerHTML = list.map((item) => {
         return `
@@ -38,4 +41,11 @@ const showCompleteList = () => {
         <br>
         `;
     });
+    total();
+};
+
+const total = () => {
+    const totalGastos = document.querySelector('#gastos');
+    const gastos = list.reduce((acc, curr) => acc + curr.quantity, 0);
+    totalGastos.innerHTML = gastos;
 };
--- modulo-1/evaluacion/readme.md
+++ modulo-1/evaluacion/readme.md
@@ -1,1 +1,37 @@
-Proyecto Bootcamp Javascript
+Evaluación 🎓
+Ya sabes que la mejor forma de aprender es practicando
+
+Lo escucho y lo olvido, lo veo y lo recuerdo, lo hago y lo entiendo.
+
+Confucio
+
+Así que te animo a que hagas la siguiente evaluación, te aseguro que te ayudará a afianzar los conocimientos que has adquirido hasta ahora.
+
+Controlador de gastos
+Nos han encargado un controlador de gastos para una aplicación de gestión de finanzas.
+
+El controlador debe permitir como mínimo:
+
+Añadir un gasto
+Mostrar un listado de gastos
+Editar un gasto
+Eliminar un gasto
+Todo esto lo vamos a hacer con JavaScript por supuesto. Como aún no sabemos manipular el DOM (próximo módulo) vamos a hacerlo todo usando la consola del navegador y usando prompt para pedir datos al usuario.
+
+Hay un par de bonus que puedes hacer:
+
+Los datos de los gastos se guardan en el localStorage para que no se pierdan al recargar la página.
+Cuando lo termines puedes incluir también los ingresos y mostrar un balance de los mismos.
+Consejos:
+
+Piensa bien la estructura de datos que vas a usar
+Ten en cuenta la experiencia de usuario (que sea "feo" no es un problema, pero que no se pueda usar sí lo es).
+Intenta hacerlo lo más modular posible, es decir, que cada función haga una cosa y lo haga bien.
+¿Qué debe incluir la evaluación?
+Un repositorio en GitHub que incluya los siguientes archivos:
+index.html Título de la página, script de JavaScript.
+index.js Todo el código JavaScript de la aplicación.
+readme.md con la descripción del proyecto, los pasos para ejecutarlo como si fuesemos un usuario (instrucciones de uso) y los comentarios sobre qué hace el código, como es el flujo de datos...etc (documentación)
+
+EXTRA
+Si quieres puedes incluir tu evaluación en el siguiente readme de GitHub: Evaluaciones
